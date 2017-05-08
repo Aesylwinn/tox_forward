e4dca886ac65af13541b1dee21e40efbcddcfff4
Kyle Cooley <kyleacooley@gmail.com>
2017-05-08 17:45:59 -0400

    Continued work on tox wrapper

--- src/toxwrapper.cpp
+++ src/toxwrapper.cpp
@@ -1,11 +1,153 @@
 #include "toxwrapper.h"
 
 #include <cassert>
+#include <chrono>
+#include <map>
+#include <thread>
 #include <sodium.h>
 #include <vector>
 
+/*! @brief Used to map callbacks to specific instances.
+ */
+class ToxWrapperRegistry
+{
+public:
+    /*! @brief Registers a specific wrapper tied to a specific instance.
+     *  @param instance The Tox instance.
+     *  @param wrapper The wrapper.
+     */
+    void registerWrapper(Tox* instance, ToxWrapper* wrapper);
+
+    /*! @brief Unregisters the wrapper tied to a specific instance.
+     *  @param instance The Tox instance to be unmapped.
+     */
+    void unregisterWrapper(Tox* instance);
+
+    /*! @brief Returns the wrapper tied to a specific instance. Assumes it
+     *         exists.
+     *  @param instance The Tox instance the wrapper has been tied to.
+     */
+    ToxWrapper* lookup(Tox* instance);
+
+    /*! @brief Returns the global instance of the ToxWrapperRegistry class
+     */
+    static ToxWrapperRegistry& get();
+
+private:
+    ToxWrapperRegistry() = default;
+    ~ToxWrapperRegistry() = default;
+
+    std::map<Tox*, ToxWrapper*> mRegistrations;
+};
+
+
+// A bunch of callbacks
+
+void self_connection_status_changed(Tox* tox, TOX_CONNECTION status,
+                                    void* user_data)
+{
+    bool online = (status != TOX_CONNECTION_NONE);
+    ToxWrapperRegistry::get().lookup(tox)->onConnectionStatusChanged(online);
+}
+
+void friend_request(Tox* tox, const uint8_t* publicKeyBin,
+                    const uint8_t* rawMessage, size_t length, void* userData)
+{
+    // Convert from binary to hexadecimal
+    std::string publicKeyHex;
+    publicKeyHex.resize(tox_public_key_size() * 2 + 1); // +1 for null character
+    assert(!sodium_bin2hex(&publicKeyHex[0], publicKeyHex.size(),
+                           &publicKeyBin[0], tox_public_key_size()));
+    publicKeyHex.pop_back(); // remove null character
+
+    // Convert message from uint8_t form
+    std::string message(rawMessage, rawMessage+length);
+
+    ToxWrapperRegistry::get().lookup(tox)->onFriendRequestRecieved(publicKeyHex,
+                                                                   message);
+}
+
+void friend_name_changed(Tox* tox, uint32_t alias, const uint8_t* rawName,
+                         size_t length, void* user_data)
+{
+    std::string name(rawName, rawName+length);
+    ToxWrapperRegistry::get().lookup(tox)->onFriendNameChanged(alias, name);
+}
+
+void friend_status_message_changed(Tox* tox, uint32_t alias,
+                                   const uint8_t* rawMessage, size_t length,
+                                   void *userData)
+{
+    std::string message(rawMessage, rawMessage+length);
+    ToxWrapperRegistry::get().lookup(tox)->
+        onFriendStatusMessageChanged(alias, message);
+}
+
+void friend_connection_status_changed(Tox* tox, uint32_t alias,
+                                      TOX_CONNECTION status, void* userData)
+{
+    bool online = (status != TOX_CONNECTION_NONE);
+    ToxWrapperRegistry::get().lookup(tox)->
+        onFriendConnectionStatusChanged(alias, online);
+}
+
+void friend_read_reciept(Tox* tox, uint32_t alias, uint32_t messageId,
+                                  void *user_data)
+{
+    ToxWrapperRegistry::get().lookup(tox)->onMessageSentSuccess(alias,
+                                                                messageId);
+}
+
+void friend_message(Tox* tox, uint32_t alias, TOX_MESSAGE_TYPE type,
+                    const uint8_t* rawMessage, size_t length, void* userData)
+{
+    std::string message(rawMessage, rawMessage+length);
+    bool actionType = (type == TOX_MESSAGE_TYPE_ACTION);
+    ToxWrapperRegistry::get().lookup(tox)->onMessageRecieved(alias, message,
+                                                             actionType);
+}
+
+
+// The ToxWrapperRegistry implementation
+
+void ToxWrapperRegistry::registerWrapper(Tox* tox, ToxWrapper* wrapper)
+{
+    // Register
+    mRegistrations[tox] = wrapper;
+
+    // Add callbacks
+    tox_callback_self_connection_status(tox, self_connection_status_changed);
+    tox_callback_friend_name(tox, friend_name_changed);
+    tox_callback_friend_status_message(tox, friend_status_message_changed);
+    tox_callback_friend_connection_status(tox,
+                                          friend_connection_status_changed);
+    tox_callback_friend_read_receipt(tox, friend_read_reciept);
+    tox_callback_friend_message(tox, friend_message);
+    tox_callback_friend_request(tox, friend_request);
+}
+
+void ToxWrapperRegistry::unregisterWrapper(Tox* instance)
+{
+    mRegistrations.erase(instance);
+}
+
+ToxWrapper* ToxWrapperRegistry::lookup(Tox* instance)
+{
+    return mRegistrations[instance];
+}
+
+ToxWrapperRegistry& ToxWrapperRegistry::get()
+{
+    static ToxWrapperRegistry instance;
+    return instance;
+}
+
+
+// The ToxWrapper implementation
+
 ToxWrapper::ToxWrapper()
     : mTox(nullptr)
+    , mStop(false)
 {
     // Defaults for now
     Tox_Options* options = tox_options_new(nullptr);
@@ -15,6 +157,9 @@ ToxWrapper::ToxWrapper()
     mTox = tox_new(options, nullptr);
     assert(mTox);
 
+    // Register
+    ToxWrapperRegistry::get().registerWrapper(mTox, this);
+
     // Clean up resources
     tox_options_free(options);
 }
@@ -23,6 +168,9 @@ ToxWrapper::~ToxWrapper()
 {
     // Destroy tox instance
     tox_kill(mTox);
+
+    // Unregister
+    ToxWrapperRegistry::get().unregisterWrapper(mTox);
 }
 
 bool ToxWrapper::bootstrapNode(const std::string& address, uint16_t port,
@@ -129,8 +277,106 @@ uint32_t ToxWrapper::addFriendNoRequest(const std::string& publicKey)
     return tox_friend_add_norequest(mTox, &publicKeyBin[0], nullptr);
 }
 
+uint32_t ToxWrapper::getFriendByPublicKey(const std::string& publicKeyHex)
+{
+    // Convert hex to binary
+    std::vector<uint8_t> publicKeyBin(tox_public_key_size(), 0);
+    assert(!sodium_hex2bin(&publicKeyBin[0], tox_public_key_size(),
+                           publicKeyHex.data(), publicKeyHex.size(), nullptr,
+                           nullptr, nullptr));
+
+    // Retrieve alias
+    return tox_friend_by_public_key(mTox, &publicKeyBin[0], nullptr);
+}
+
+bool ToxWrapper::friendExists(uint32_t alias)
+{
+    return tox_friend_exists(mTox, alias);
+}
+
 bool ToxWrapper::deleteFriend(uint32_t alias)
 {
     // They must be DESTROYED!!!
     return tox_friend_delete(mTox, alias, nullptr);
+}
+
+bool ToxWrapper::isFriendConnected(uint32_t alias)
+{
+    // Check if the friend is online.
+    TOX_CONNECTION status;
+    status = tox_friend_get_connection_status(mTox, alias, nullptr);
+
+    return (status != TOX_CONNECTION_NONE);
+}
+
+uint32_t ToxWrapper::sendMessage(uint32_t friendAlias, std::string message,
+                                 bool actionType)
+{
+    // Select message type
+    TOX_MESSAGE_TYPE messageType = (actionType) ? TOX_MESSAGE_TYPE_ACTION :
+                                                  TOX_MESSAGE_TYPE_NORMAL;
+
+    // Convert message fo uint8_t form
+    std::vector<uint8_t> rawMessage(message.begin(), message.end());
+
+    // Send the message
+    return tox_friend_send_message(mTox, friendAlias, messageType,
+                                   &rawMessage[0], rawMessage.size(), nullptr);
+}
+
+void ToxWrapper::onConnectionStatusChanged(bool online)
+{
+}
+
+void ToxWrapper::onFriendRequestRecieved(const std::string& publicKey,
+                                         const std::string& message)
+{
+}
+
+void ToxWrapper::onFriendNameChanged(uint32_t alias, const std::string& name)
+{
+}
+
+void ToxWrapper::onFriendStatusMessageChanged(uint32_t alias,
+                                              const std::string& message)
+{
+}
+
+void ToxWrapper::onFriendConnectionStatusChanged(uint32_t alias, bool online)
+{
+}
+
+void ToxWrapper::onMessageSentSuccess(uint32_t friendAlias, uint32_t messageId)
+{
+}
+
+void ToxWrapper::onMessageRecieved(uint32_t friendAlias,
+                                   const std::string& message, bool actionType)
+{
+}
+
+void ToxWrapper::onCoreUpdate()
+{
+}
+
+void ToxWrapper::run()
+{
+    mStop = false;
+    while (!mStop)
+    {
+        // Wait until the next update is required
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(tox_iteration_interval(mTox)));
+
+        // Let tox do its work
+        tox_iterate(mTox, nullptr);
+
+        // Callback
+        onCoreUpdate();
+    }
+}
+
+void ToxWrapper::stop()
+{
+    mStop = true;
 }
--- src/toxwrapper.h
+++ src/toxwrapper.h
@@ -4,7 +4,8 @@
 #include <string>
 #include <tox/tox.h>
 
-/*! @brief Wraps a tox instance.
+/*! @brief Wraps a tox instance. This is based largely off of the tox api, look
+ *         there for more documentation.
  */
 class ToxWrapper
 {
@@ -16,7 +17,7 @@ public:
 
     /*! @brief Destroys the associated tox instance.
      */
-    ~ToxWrapper();
+    virtual ~ToxWrapper();
 
 
     /*! @brief Attemps to connect to a node, see the tox documentation.
@@ -75,17 +76,105 @@ public:
      */
     uint32_t addFriendNoRequest(const std::string& publicKey);
 
+    /*! @brief Returns the alias for a specific friend.
+     *  @param publicKey The public key of the friend in hexadecimal.
+     *  @return The alias for the friend if succussful, otherwise UINT32_MAX.
+     */
+    uint32_t getFriendByPublicKey(const std::string& publicKey);
+
+    /*! @brief Returns whether an alias has been mapped to a friend. Can be used
+     *         for validation.
+     *  @param alias The alias to check.
+     *  @return True if the the friend exists.
+     */
+    bool friendExists(uint32_t alias);
+
     /*! @brief Removes the friend from the friend list.
      *  @param alias The alias for the friend.
      *  @return True on success.
      */
     bool deleteFriend(uint32_t alias);
 
-    // TODO at line 1300
+
+    /*! @brief Returns whether or not a specific friend is online.
+     *  @param alias
+     *  @return True if the friend is connected.
+     */
+    bool isFriendConnected(uint32_t alias);
+
+    /*! @brief Sends a message to a specific friend.
+     *  @param friendAlias The alias for a friend.
+     *  @param message The message. Must be shorter than TOX_MAX_MESSAGE_LENGTH.
+     *  @param actionType Whether or not the message is an action (/me for ex.).
+     *  @return The unique message identifier for the given friend. Can be used
+     *          to verify the message sent was recieved.
+     */
+    uint32_t sendMessage(uint32_t friendAlias, std::string message,
+                         bool actionType=false);
+
+
+    /*! @brief Called when the connection status changes.
+     *  @param online True if we are online.
+     */
+    virtual void onConnectionStatusChanged(bool online);
+
+    /*! @brief Called when a friend request is recieved.
+     *  @param publicKey The public key of the sender in hexadecimal.
+     *  @param message The message sent with the request.
+     */
+    virtual void onFriendRequestRecieved(const std::string& publicKey,
+                                         const std::string& message);
+
+    /*! @brief Called when a friend's name is changed.
+     *  @param alias The alias for the friend.
+     *  @param name The new name.
+     */
+    virtual void onFriendNameChanged(uint32_t alias, const std::string& name);
+
+    /*! @brief Called when a friend's status message is changed.
+     *  @param alias The alias for the friend.
+     *  @param message The new status message.
+     */
+    virtual void onFriendStatusMessageChanged(uint32_t alias,
+                                              const std::string& message);
+
+    /*! @brief Called when the connection status of a friend is changed.
+     *  @param alias The alias for the friend.
+     *  @param online True if the friend is online.
+     */
+    virtual void onFriendConnectionStatusChanged(uint32_t alias, bool online);
+
+    /*! @brief Called when the reciept for a sent message is recieved.
+     *  @param friendAlias The alias for the friend the message was sent to.
+     *  @param messageId The unique id (for the friend) of the message sent.
+     */
+    virtual void onMessageSentSuccess(uint32_t friendAlias, uint32_t messageId);
+
+    /*! @brief Called when a message from a friend is recieved.
+     *  @param friendAlias The alias for the friend.
+     *  @param message The message recieved.
+     *  @param actionType Whether or not the message is an action (/me for ex.).
+     */
+    virtual void onMessageRecieved(uint32_t friendAlias,
+                                   const std::string& message, bool actionType);
+
+    /*! @brief Called after each update to the Tox instance.
+     */
+    virtual void onCoreUpdate();
+
+
+    /*! @brief Executes main loop for Tox instance. Currently not thread safe.
+     */
+    void run();
+
+    /*! @brief Stops the main loop started by calling run().
+     */
+    void stop();
 
 private:
 
     Tox* mTox;
+    bool mStop;
 };
 
 #endif
