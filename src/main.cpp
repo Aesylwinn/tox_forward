6c52239394feeddda7c0aaf17f90369739773758
Kyle Cooley <kyleacooley@gmail.com>
2017-05-07 22:22:22 -0400

    Fix some formatting issues.

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,12 @@ using namespace std::chrono;
 using namespace std::this_thread;
 
 
+
 // Forward Declarations
 
 void handle_friend_request(Tox*, const uint8_t*, const uint8_t*, size_t, void*);
 void handle_friend_message(Tox*, uint32_t, TOX_MESSAGE_TYPE, const uint8_t*,
-		           size_t, void*);
+                           size_t, void*);
 
 struct DHTNode
 {
@@ -37,7 +38,7 @@ int main(int argc, const char* argv[])
     if (error != TOX_ERR_NEW_OK)
     {
         cout << "Failed to initialize tox." << endl;
-	return -1;
+        return -1;
     }
 
     // Callbacks
@@ -49,14 +50,21 @@ int main(int argc, const char* argv[])
 
     DHTNode nodes[] =
     {
-        { "biribiri.org", 33445, "F404ABAA1C99A9D37D61AB54898F56793E1DEF8BD46B1038B9D822E8460FAB67", {0} }
+        {
+            "biribiri.org",
+            33445,
+            "F404ABAA1C99A9D37D61AB54898F56793E1DEF8BD46B1038B9D822E8460FAB67",
+            {0}
+        }
     };
 
     for (size_t i = 0; i < sizeof(nodes)/sizeof(DHTNode); ++i)
     {
         sodium_hex2bin(nodes[i].binKey, sizeof(nodes[i].binKey),
-                       nodes[i].hexKey, sizeof(nodes[i].hexKey)-1, NULL, NULL, NULL);
-        tox_bootstrap(tox, nodes[i].ip.c_str(), nodes[i].port, nodes[i].binKey, NULL);
+                       nodes[i].hexKey, sizeof(nodes[i].hexKey)-1, NULL, NULL,
+                       NULL);
+        tox_bootstrap(tox, nodes[i].ip.c_str(), nodes[i].port, nodes[i].binKey,
+                      NULL);
     }
 
     // Print address
@@ -75,15 +83,16 @@ int main(int argc, const char* argv[])
     while (!g_exit)
     {
         sleep_for(milliseconds(tox_iteration_interval(tox)));
-	tox_iterate(tox, NULL);
+        tox_iterate(tox, NULL);
     }
 
 
     return 0;
 }
 
-void handle_friend_request(Tox* tox, const uint8_t* publicKey, const uint8_t* message,
-                                  size_t length, void* userData)
+void handle_friend_request(Tox* tox, const uint8_t* publicKey,
+                           const uint8_t* message, size_t length,
+                           void* userData)
 {
     // Add friend
 
@@ -97,16 +106,15 @@ void handle_friend_request(Tox* tox, const uint8_t* publicKey, const uint8_t* me
     }
 }
 
-void handle_friend_message(Tox* tox, uint32_t friendNum,
-                                  TOX_MESSAGE_TYPE type,
-		                  const uint8_t* message,
-				  size_t length, void* userData)
+void handle_friend_message(Tox* tox, uint32_t friendNum, TOX_MESSAGE_TYPE type,
+                           const uint8_t* message, size_t length,
+                           void* userData)
 {
     // Check for quit message
     if (length == 4 && string(message, message + length) == "quit")
     {
         g_exit = true;
-	return;
+        return;
     }
 
     // Echo message
